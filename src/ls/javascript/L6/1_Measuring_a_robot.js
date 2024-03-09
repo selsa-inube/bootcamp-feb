919ea4e7ff4b6df7548950e81e4cc7cf1ce596fb
Luis Alejandro Salazar <luisalejandrosalazar@gmail.com>
2024-03-09 09:34:22 -0500

    fix: 2_Robot_efficiency updated

--- src/ls/javascript/L6/1_Measuring_a_robot.js
+++ src/ls/javascript/L6/1_Measuring_a_robot.js
@@ -1,28 +1,33 @@
-const { routeRobot, goalOrientedRobot, VillageState } = require('./robot');
-
+const { routeRobot, goalOrientedRobot, VillageState } = require("./robot");
 
 function countSteps(state, robot, memory) {
-    for (let steps = 0; ; steps++) {
-        if (state.parcels.length == 0) {
-            return steps;
-        } else {
-            let action = robot(state, memory);
-            state = state.move(action.direction);
-            memory = action.memory;
-        }
+  for (let steps = 0; ; steps++) {
+    if (state.parcels.length == 0) {
+      return steps;
+    } else {
+      let action = robot(state, memory);
+      state = state.move(action.direction);
+      memory = action.memory;
     }
+  }
 }
 
 function compareRobots(robot1, memory1, robot2, memory2) {
-    let stepsR1 = 0;
-    let stepsR2 = 0;
-    for (let i = 0; i < 100; i++) {
-        let state = VillageState.random();
-        stepsR1 += countSteps(state, robot1, memory1);
-        stepsR2 += countSteps(state, robot2, memory2);
-    }
-    console.log('Average Robot 1 = ' + (stepsR1 / 100).toFixed(2))
-    console.log('Average Robot 2 = ' + (stepsR2 / 100).toFixed(2))
+  let stepsR1 = 0;
+  let stepsR2 = 0;
+  for (let i = 0; i < 1; i++) {
+    let state = VillageState.random();
+    console.log("state ===>");
+    console.log(state);
+    stepsR1 += countSteps(state, robot1, memory1);
+    stepsR2 += countSteps(state, robot2, memory2);
+  }
+  console.log("Average Robot 1 = " + (stepsR1 / 100).toFixed(2));
+  console.log("Average Robot 2 = " + (stepsR2 / 100).toFixed(2));
 }
 
-compareRobots(routeRobot, [], goalOrientedRobot, []);
+// compareRobots(routeRobot, [], goalOrientedRobot, []);
+
+module.exports = {
+  compareRobots,
+};
--- src/ls/javascript/L6/2_Robot_efficiency.js
+++ src/ls/javascript/L6/2_Robot_efficiency.js
@@ -1,27 +1,59 @@
-const { routeRobot, goalOrientedRobot, VillageState } = require('./robot');
+const {
+  findRoute,
+  roadGraph,
+  routeRobot,
+  goalOrientedRobot,
+} = require("./robot");
+const { compareRobots } = require("./1_Measuring_a_robot");
 
-function xxxgoalOrientedRobot({ place, parcels }, route) {
-    if (route.length == 0) {
-        let parcel = parcels[0];
-        if (parcel.place != place) {
-            route = findRoute(roadGraph, place, parcel.place);
-        } else {
-            route = findRoute(roadGraph, place, parcel.address);
-        }
+function myRobot({ place, parcels }, route) {
+  function distance(graph, from, to) {
+    return findRoute(graph, from, to).length;
+  }
+  console.log("route.length = " + route.length);
+  // Si la ruta está vacía, determinamos la mejor ruta para un paquete
+  if (route.length == 0) {
+    let shortestRoute = 0;
+    let shortestDistance = Infinity;
+
+    // Iteramos sobre todos los paquetes para encontrar el más cercano
+    for (let parcel of parcels) {
+      let routeTemp = null;
+      console.log("------------------");
+      console.log("place = " + place);
+      console.log("parcel = " + parcel);
+      console.log("parcel.place = " + parcel.place);
+      console.log("parcel.address = " + parcel.address);
+
+      if (parcel.place != place) {
+        routeTemp = findRoute(roadGraph, place, parcel.place);
+      } else {
+        routeTemp = findRoute(roadGraph, place, parcel.address);
+      }
+
+      console.log("routeTemp = " + routeTemp);
+      console.log("routeTemp.length  = " + routeTemp.length);
+
+      // Calculamos la distancia total de esta ruta
+      let totalDistance =
+        routeTemp.length + distance(roadGraph, parcel.place, parcel.address);
+
+      // Actualizamos la ruta más corta si es necesario
+      if (totalDistance < shortestDistance) {
+        shortestRoute = routeTemp.concat(
+          findRoute(roadGraph, parcel.place, parcel.address),
+        );
+        shortestDistance = totalDistance;
+      }
     }
-    return { direction: route[0], memory: route.slice(1) };
-};
 
-function runRobotAnimation({ place, parcels }, route) {
-    if (route.length == 0) {
-        let parcel = parcels[0];
-        if (parcel.place != place) {
-            route = findRoute(roadGraph, place, parcel.place);
-        } else {
-            route = findRoute(roadGraph, place, parcel.address);
-        }
-    }
-    return { direction: route[0], memory: route.slice(1) };
-};
+    // Asignamos la mejor ruta encontrada
+    route = shortestRoute;
+  }
 
-runRobotAnimation(VillageState.random(), yourRobot, memory);
+  // Devolvemos la dirección y la memoria según la ruta calculada
+  return { direction: route[0], memory: route.slice(1) };
+}
+
+compareRobots(goalOrientedRobot, [], myRobot, []);
+//runRobotAnimation(VillageState.random(), yourRobot, memory);
