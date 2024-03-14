d8d2ff1072eff3909cb4f1b6bea333520729489e
Luis Alejandro Salazar <luisalejandrosalazar@gmail.com>
2024-03-13 20:00:31 -0400

    fix: index.css and css references deleted

--- src/ls/react/A2P_NYT/src/App.jsx
+++ src/ls/react/A2P_NYT/src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <section>Aside</section>
-      <section>Header</section>
-      <section>Content</section>
-      <section>Footer</section>
+      <aside>Aside</aside>
+      <header>Header</header>
+      <main>Main</main>
+      <footer>Footer</footer>
     </>
   );
 }
