6caa22a47fa0e9133da667ed7ded5d412919e4e5
Luis Alejandro Salazar <luisalejandrosalazar@gmail.com>
2024-03-20 13:27:03 -0400

    fix: issue in 2_The_locked_box.js file,         added the functionality for extra points

--- src/ls/javascript/L8/2_The_locked_box.js
+++ src/ls/javascript/L8/2_The_locked_box.js
@@ -15,11 +15,12 @@ const box = new (class {
 })();
 
 function withBoxUnlocked(body) {
-  box.unlock();
+  let boxStatus = box.locked;
+  if (boxStatus) box.unlock();
   try {
     return body();
   } finally {
-    box.lock();
+    if (boxStatus) box.lock();
   }
 }
 
@@ -40,3 +41,20 @@ withBoxUnlocked(() => {
 });
 
 console.log(box.locked);
+
+//extra points
+console.log("\n-- extra points --\n");
+console.log("Initial status blocked: " + box.locked);
+withBoxUnlocked(() => {
+  console.log(" - First call of withBoxUnlocked() to unlock the box");
+  console.log(" - Status: blocked = " + box.locked);
+  withBoxUnlocked(() => {
+    console.log(
+      " - Second call of withBoxUnlocked() while the box is unlocked",
+    );
+  });
+  console.log(" - Status after second call: blocked = " + box.locked);
+  console.log(" - " + box.content);
+});
+
+console.log("Final status blocked: " + box.locked);
